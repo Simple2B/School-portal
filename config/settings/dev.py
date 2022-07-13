dae985c8bb51203875fb1f63b861e96909f29de0
Aleksandr <amvorobei@gmail.com>
2022-07-14 01:31:15 +0300

    Generate scaffold

---
+++ config/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-i@zoun6tf6)vr(s=_=$0o4z=^_uo)ornc#=wqm-_e7+s(jle^g"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
