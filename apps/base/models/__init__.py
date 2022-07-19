f7f12e086fbbe698a56b592b1ead22edb763457a
Aleksandr <amvorobei@gmail.com>
2022-07-19 14:24:21 +0300

    Optimization base models

---
+++ apps/base/models/__init__.py
@@ -0,0 +1,4 @@
+from .pages import *
+
+# from settings import *
+# from snippets import *
--- apps/base/models/pages.py
+++ apps/base/models/pages.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from wagtail.models import Page
-from wagtail.fields import StreamField, RichTextField
+from wagtail.fields import StreamField
 from wagtail.core.blocks import RichTextBlock, CharBlock
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.embeds.blocks import EmbedBlock
