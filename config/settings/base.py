c27dffca4772991c19ceb6c1259ec989a5587810
Aleksandr <amvorobei@gmail.com>
2022-07-27 11:35:34 +0300

    Update static

--- config/settings/base.py
+++ config/settings/base.py
@@ -151,7 +151,7 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
