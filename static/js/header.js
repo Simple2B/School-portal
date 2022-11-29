7a40c9ff793a3ecc670a0ce790b7128a492cf697
denys <denysburimov@gmai.com>
2022-11-29 17:28:44 +0200

    work

--- static/js/header.js
+++ static/js/header.js
@@ -3,11 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const mobileMenuButton = document.querySelector('.mobile_menu_button');
     const webHeader = document.querySelector('.header_wrapper');
     const mobileMenu = document.querySelector('.mobile_menu');
+    const contentSection = document.querySelector('.display_content');
+    const footer = document.querySelector('.footer');
 
     mobileMenuButton.addEventListener("click", function(event) {
         mobileMenuButton.style.display = "none";
         logo.style.display = "none";
         webHeader.style.display = "none";
+        contentSection.style.display = "none";
+        footer.style.display = "none";
+        document.body.style.backgroundColor = "#1d1b4c";
         mobileMenu.style.display = "block";
     });
 });
