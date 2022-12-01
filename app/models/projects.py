9fae04d28389fb9d336976d16835de58ce88aa47
denys <denysburimov@gmai.com>
2022-12-02 00:00:32 +0200

    projects page

---
+++ app/models/projects.py
@@ -0,0 +1,38 @@
+from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import StreamField
+from django.db import models
+
+from app.blocks.base_stream_block import BaseStreamBlock
+
+
+class ProjectsPage(Page):
+    def get_context(self, request):
+        context = super().get_context(request)
+        context["projects"] = [
+            "FastAPI Project",
+            "JS Project",
+            "Django Project",
+            "ML Project",
+        ]
+
+        print(context)
+        return context
+
+    question = models.CharField(max_length=255, default="")
+    message = models.CharField(max_length=255, default="")
+    phrase = models.CharField(max_length=255, default="")
+
+    body = StreamField(
+        BaseStreamBlock(),
+        verbose_name="Page body",
+        blank=True,
+        use_json_field=True,
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("question"),
+        FieldPanel("message"),
+        FieldPanel("phrase"),
+        FieldPanel("body"),
+    ]
