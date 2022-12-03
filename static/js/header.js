b8daf55ad05625d511bf51f4fff17ba9e5be550c
denys <denysburimov@gmai.com>
2022-12-03 14:02:44 +0200

    desktop menu highlighting

--- static/js/header.js
+++ static/js/header.js
@@ -15,4 +15,38 @@ document.addEventListener('DOMContentLoaded', function() {
         document.body.style.backgroundColor = "#1d1b4c";
         mobileMenu.style.display = "block";
     });
+
+    const allHighlights = document.querySelectorAll('.highlight');
+    const solutions_button = document.querySelector('.solutions_button');
+    const solutions_highlight = document.querySelector('.solutions_highlight');
+
+    const projects_button = document.querySelector('.projects_button');
+    const projects_highlight = document.querySelector('.projects_highlight');
+    
+    const careers_button = document.querySelector('.careers_button');
+    const careers_highlight = document.querySelector('.careers_highlight');
+
+    console.log(allHighlights.length)
+
+    function disableAllHiglights() {
+        for (let i = 0; i < allHighlights.length; i++) {
+            console.log(allHighlights[i], allHighlights[i].style.backgroundColor)
+            allHighlights[i].style.display = "none";
+        }
+    }
+
+    solutions_button.addEventListener("click", function(event) {
+        disableAllHiglights();
+        solutions_highlight.style.display = "block";
+    });
+
+    projects_button.addEventListener("click", function(event) {
+        disableAllHiglights();
+        projects_highlight.style.display = "block";
+    });
+    
+    careers_button.addEventListener("click", function(event) {
+        disableAllHiglights();
+        careers_highlight.style.display = "block";
+    });
 });
